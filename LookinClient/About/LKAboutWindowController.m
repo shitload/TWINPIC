2f3898735729d0cfce0f86f7938637caefa94dfe
shitload <111417421+shitload@users.noreply.github.com>
2020-05-01 13:46:52 +0000

    added docstrings

--- LookinClient/About/LKAboutWindowController.m
+++ LookinClient/About/LKAboutWindowController.m
@@ -2,4 +2,24 @@
 //  LKAboutWindowController.m
 //  LookinClient
 //
-//  Create
+//  Created by 李凯 on 2019/10/30.
+//  Copyright © 2019 hughkli. All rights reserved.
+//
+
+#import "LKAboutWindowController.h"
+#import "LKAboutViewController.h"
+#import "LKWindow.h"
+
+@interface LKAboutWindowController ()
+
+@end
+
+@implementation LKAboutWindowController
+
+- (instancetype)init {
+    CGFloat width = 500;
+    CGFloat height = width * 0.54;
+    
+    LKWindow *window = [[LKWindow alloc] initWithContentRect:NSMakeRect(0, 0, width, height) styleMask:NSWindowStyleMaskTitled|NSWindowStyleMaskClosable backing:NSBackingStoreBuffered defer:YES];
+    window.movableByWindowBackground = YES;
+ 
