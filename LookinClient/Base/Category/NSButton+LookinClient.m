f6e79a3dbacc762e2bca5fe6155519edcc0bf307
shitload <111417421+shitload@users.noreply.github.com>
2020-07-08 09:23:37 +0000

    [GitLab CI] Updated translation

--- LookinClient/Base/Category/NSButton+LookinClient.m
+++ LookinClient/Base/Category/NSButton+LookinClient.m
@@ -15,4 +15,20 @@
     button.bezelStyle = NSBezelStyleRounded;
     button.title = title;
     button.font = [NSFont systemFontOfSize:13];
-    button.target = ta
+    button.target = target;
+    button.action = action;
+    button.frame = NSMakeRect(0, 0, 84, 40);
+    return button;
+}
+
++ (instancetype)lk_buttonWithImage:(NSImage *)image target:(id)target action:(SEL)action {
+    NSButton *button = [NSButton new];
+    button.image = image;
+    button.bezelStyle = NSBezelStyleRoundRect;
+    button.bordered = NO;
+    button.target = target;
+    button.action = action;
+    return button;
+}
+
+@end
---
+++ LookinClient/Base/Category/NSColor+LookinClient.h
@@ -0,0 +1,11 @@
+//
+//  NSColor+LookinClient.h
+//  Lookin
+//
+//  Created by Li Kai on 2019/5/17.
+//  https://lookin.work
+//
+
+#import <Cocoa/Cocoa.h>
+
+@i
