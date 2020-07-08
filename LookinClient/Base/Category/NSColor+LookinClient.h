edda9817443e7d577ee491a345a63ecad612d660
shitload <111417421+shitload@users.noreply.github.com>
2020-07-08 21:11:51 +0000

    never mind

--- LookinClient/Base/Category/NSColor+LookinClient.h
+++ LookinClient/Base/Category/NSColor+LookinClient.h
@@ -8,4 +8,18 @@
 
 #import <Cocoa/Cocoa.h>
 
-@i
+@interface NSColor (LookinClient)
+
+/**
+ 如果 alpha 为 1 则返回诸如 (15, 17, 19) 这样的格式
+ 如果 alpha 小于 1 则返回诸如 (15, 17, 19, 0.5) 这样的格式
+ */
+- (NSString *)rgbaString;
+
+- (NSString *)hexString;
+
+- (NSArray<NSNumber *> *)lk_rgbaComponents;
+
++ (instancetype)lk_colorFromRGBAComponents:(NSArray<NSNumber *> *)components;
+
+@end
---
+++ LookinClient/Base/Category/NSControl+LookinClient.h
@@ -0,0 +1,10 @@
+//
+//  NSControl+LookinClient.h
+//  Lookin
+//
+//  Created by Li Kai on 2019/8/13.
+//  https://lookin.work
+//
+
+#import <AppKit/AppKit.h>
+#impo
