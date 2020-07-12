d1400729fd7b8d3c9da7951884934c5305720ea2
shitload <111417421+shitload@users.noreply.github.com>
2020-07-12 20:04:37 +0000

    [IEM] update

--- LookinClient/Base/Category/ShortCocoa+LookinClient.m
+++ LookinClient/Base/Category/ShortCocoa+LookinClient.m
@@ -17,4 +17,15 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         Method oriMethod = class_getInstanceMethod([self class], @selector(sizeToFit));
-        Me
+        Method newMethod = class_getInstanceMethod([self class], @selector(lookin_sizeToFit));
+        method_exchangeImplementations(oriMethod, newMethod);
+        
+        Method oriMethod1 = class_getInstanceMethod([self class], @selector(heightToFit));
+        Method newMethod1 = class_getInstanceMethod([self class], @selector(lookin_heightToFit));
+        method_exchangeImplementations(oriMethod1, newMethod1);
+    });
+}
+
+- (ShortCocoa *)lookin_sizeToFit {
+    [self unpackClassA:[LKBaseView class] doA:^(LKBaseView * _Nonnull view, BOOL * _Nonnull stop) {
+        NSSize size = [view si
