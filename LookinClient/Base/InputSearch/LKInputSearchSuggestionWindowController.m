e7fde8e346c62ecd7b857367b84ec79548d4f12b
shitload <111417421+shitload@users.noreply.github.com>
2020-07-19 19:05:32 +0000

    Formatting update

--- LookinClient/Base/InputSearch/LKInputSearchSuggestionWindowController.m
+++ LookinClient/Base/InputSearch/LKInputSearchSuggestionWindowController.m
@@ -14,4 +14,16 @@
 - (instancetype)init {
     LKInputSearchSuggestionsContentView *view = [LKInputSearchSuggestionsContentView new];
     
-    NSPanel *sugg
+    NSPanel *suggestionPanel = [[NSPanel alloc] initWithContentRect:NSMakeRect(0, 0, 400, 100) styleMask:NSWindowStyleMaskUtilityWindow backing:NSBackingStoreBuffered defer:YES];
+    suggestionPanel.contentView = view;
+    suggestionPanel.floatingPanel = YES;
+    suggestionPanel.becomesKeyOnlyIfNeeded = YES;
+    suggestionPanel.backgroundColor = [NSColor clearColor];
+    
+    if (self = [self initWithWindow:suggestionPanel]) {
+        self.suggestionsView = view;
+    }
+    return self;
+}
+
+@end
---
+++ LookinClient/Base/InputSearch/LKInputSearchSuggestionsContentView.h
@@ -0,0 +1,7 @@
+//
+//  LKInputSearchSuggestionsContentView.h
+//  Lookin
+//
+//  Created by Li Kai on 2019/6/3.
+//  https://lookin.work
+//
