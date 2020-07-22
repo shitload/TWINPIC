3eb11c47efac7360d0b438899ceea33e3b34ed9a
shitload <111417421+shitload@users.noreply.github.com>
2020-07-22 18:27:22 +0000

    Working on dashboard

--- LookinClient/Base/InputSearch/LKInputSearchSuggestionsRowView.h
+++ LookinClient/Base/InputSearch/LKInputSearchSuggestionsRowView.h
@@ -5,3 +5,14 @@
 //  Created by Li Kai on 2019/6/3.
 //  https://lookin.work
 //
+
+#import <Cocoa/Cocoa.h>
+
+@interface LKInputSearchSuggestionsRowView : NSTableRowView
+
+@property(nonatomic, strong, readonly) LKLabel *titleLabel;
+@property(nonatomic, strong, readonly) NSImageView *imageView;
+
+- (CGFloat)bestWidth;
+
+@end
---
+++ LookinClient/Base/InputSearch/LKInputSearchSuggestionsRowView.m
@@ -0,0 +1,16 @@
+//
+//  LKInputSearchSuggestionsRowView.m
+//  Lookin
+//
+//  Created by Li Kai on 2019/6/3.
+//  https://lookin.work
+//
+
+#import "LKInputSearchSuggestionsRowView.h"
+
+@implementation LKInputSearchSuggestionsRowView {
+    CGFloat _horInset;
+    CGFloat _titleLeft;
+}
+
+- (instancetype)initWithFrame:(NSRect)fr
