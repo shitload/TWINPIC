b53de655d40cba241c0339fb8bf3212f99a99d47
shitload <111417421+shitload@users.noreply.github.com>
2020-07-22 11:31:18 +0000

    ocfs2: _really_ sync the right range

--- LookinClient/Base/InputSearch/LKInputSearchSuggestionsContentView.h
+++ LookinClient/Base/InputSearch/LKInputSearchSuggestionsContentView.h
@@ -5,3 +5,19 @@
 //  Created by Li Kai on 2019/6/3.
 //  https://lookin.work
 //
+
+#import "LKBaseView.h"
+
+@class LKInputSearchSuggestionItem;
+
+@interface LKInputSearchSuggestionsContentView : LKBaseView
+
+@property(nonatomic, copy) NSArray<LKInputSearchSuggestionItem *> *items;
+
+@property(nonatomic, strong, readonly) NSTableView *tableView;
+
+- (LKInputSearchSuggestionItem *)currentSelectedItem;
+
+- (NSSize)bestSize;
+
+@end
---
+++ LookinClient/Base/InputSearch/LKInputSearchSuggestionsRowView.h
@@ -0,0 +1,7 @@
+//
+//  LKInputSearchSuggestionsRowView.h
+//  Lookin
+//
+//  Created by Li Kai on 2019/6/3.
+//  https://lookin.work
+//
