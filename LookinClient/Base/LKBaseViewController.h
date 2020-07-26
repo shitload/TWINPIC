bae011bb35028681ec286a132eeb7e7069692d47
shitload <111417421+shitload@users.noreply.github.com>
2020-07-26 20:12:56 +0000

    Removed debug

--- LookinClient/Base/LKBaseViewController.h
+++ LookinClient/Base/LKBaseViewController.h
@@ -13,4 +13,22 @@
 @interface LKBaseViewController : NSViewController
 
 /**
- 如果使用该初始化方法，则 controller.view
+ 如果使用该初始化方法，则 controller.view 会被赋值为传入的 view。
+ 如果使用普通的 init 方法，或该方法传入 nil，则 controller 会自动创建一个 view，即 [self containerView]
+ */
+- (instancetype)initWithContainerView:(NSView *)view NS_DESIGNATED_INITIALIZER;
+
+@property(nonatomic, assign, readonly) BOOL isViewAppeared;
+
+@property(nonatomic, strong, readonly) LKRedTipsView *connectionTipsView;
+
+@end
+
+@interface LKBaseViewController (NSSubclassingHooks)
+
+- (NSView *)makeContainerView;
+
+/// 是否在连接断开时显示 tips，默认为 NO
+- (BOOL)shouldShowConnectionTips;
+
+@end
