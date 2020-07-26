4045f69ce4512aa55facfb4b2613cf4ae110447b
shitload <111417421+shitload@users.noreply.github.com>
2020-07-26 19:17:31 +0000

    Update Application.java

--- LookinClient/Base/LKBaseControl.m
+++ LookinClient/Base/LKBaseControl.m
@@ -32,3 +32,61 @@
         self.alphaValue = .8;
     }
 }
+
+- (void)mouseUp:(NSEvent *)event {
+    [super mouseUp:event];
+    [self triggerClickAction];
+    if (self.adjustAlphaWhenClick) {
+        self.alphaValue = 1;
+    }
+}
+
+- (void)addTarget:(id)target clickAction:(SEL)action {
+    self.target = target;
+    self.clickAction = action;
+}
+
+- (void)triggerClickAction {
+    if (self.clickAction && self.target) {
+        [self sendAction:self.clickAction to:self.target];
+    }
+}
+
+- (void)viewDidChangeEffectiveAppearance {
+    [self _triggerDidChangeAppearanceBlock];
+}
+
+- (void)setDidChangeAppearance:(void (^)(LKBaseControl *, BOOL))didChangeAppearance {
+    _didChangeAppearance = didChangeAppearance;
+    [self _triggerDidChangeAppearanceBlock];
+}
+
+- (void)_triggerDidChangeAppearanceBlock {
+    if (self.didChangeAppearance) {
+        BOOL isDarkMode = [self.effectiveAppearance lk_isDarkMode];
+        self.didChangeAppearance(self, isDarkMode);
+    }
+}
+
+- (void)sizeToFit {
+    $(self).size([self bestSize]);
+}
+
+- (BOOL)shouldTrackMouseEnteredAndExited {
+    return NO;
+}
+
+- (void)updateTrackingAreas {
+    [super updateTrackingAreas];
+    if (![self shouldTrackMouseEnteredAndExited]) {
+        return;
+    }
+    [self.trackingAreas enumerateObjectsUsingBlock:^(NSTrackingArea * _Nonnull oldArea, NSUInteger idx, BOOL * _Nonnull stop) {
+        [self removeTrackingArea:oldArea];
+    }];
+    
+    NSTrackingArea *newArea = [[NSTrackingArea alloc] initWithRect:self.bounds options:NSTrackingMouseEnteredAndExited|NSTrackingActiveInKeyWindow|NSTrackingInVisibleRect owner:self userInfo:nil];
+    [self addTrackingArea:newArea];
+}
+
+@end
---
+++ LookinClient/Base/LKBaseViewController.h
@@ -0,0 +1,16 @@
+//
+//  LKBaseViewController.h
+//  Lookin
+//
+//  Created by Li Kai on 2018/8/28.
+//  https://lookin.work
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class LKRedTipsView;
+
+@interface LKBaseViewController : NSViewController
+
+/**
+ 如果使用该初始化方法，则 controller.view
