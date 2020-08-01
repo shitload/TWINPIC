167abdb19fc39e22c6c3a2e37c8fda0ccec3bb3d
shitload <111417421+shitload@users.noreply.github.com>
2020-08-01 06:44:26 +0000

    updated port

--- LookinClient/Base/LKHierarchyDataSource.m
+++ LookinClient/Base/LKHierarchyDataSource.m
@@ -317,3 +317,364 @@
         
     } else if (index == 3) {
         // 展开几乎全部，把 UITabBar、UINavigationBar 收起，把那些 UIButton, UILabel 等常用系统控件收起
+        NSString *Key_ExpansionDetermined = @"Key_ExpansionDetermined";
+        
+        [self.flatItems enumerateObjectsUsingBlock:^(LookinDisplayItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if (!obj.isExpandable) {
+                return;
+            }
+            if ([obj lookin_getBindBOOLForKey:Key_ExpansionDetermined]) {
+                return;
+            }
+            
+            NSNumber *prevState = referenceDict[@(obj.layerObject.oid)];
+            if (prevState) {
+                // 旧的对象，直接维持之前的状态
+                obj.isExpanded = [prevState boolValue];
+                return;
+            }
+            
+            BOOL shouldCollapseSelfAndChildren = NO;
+            if (obj.isHidden || obj.alpha <= 0) {
+                // 把 hidden, noPreview 都折叠起来
+                shouldCollapseSelfAndChildren = YES;
+                
+            } else if ([obj isKindOfClass:[LookinStaticDisplayItem class]] && ((LookinStaticDisplayItem *)obj).noPreview) {
+                shouldCollapseSelfAndChildren = YES;
+                
+            } else if (obj.preferToBeCollapsed) {
+                shouldCollapseSelfAndChildren = YES;
+                
+            } else if ([obj.title isEqualToString:@"UINavigationBar"] || [obj.title isEqualToString:@"UITabBar"]) {
+                shouldCollapseSelfAndChildren = YES;
+                
+            }
+            if (shouldCollapseSelfAndChildren) {
+                [obj enumerateSelfAndChildren:^(LookinDisplayItem *item) {
+                    item.isExpanded = NO;
+                    [item lookin_bindBOOL:YES forKey:Key_ExpansionDetermined];
+                }];
+                return;
+            }
+            obj.isExpanded = YES;
+        }];
+        
+        [self.flatItems enumerateObjectsUsingBlock:^(LookinDisplayItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [obj lookin_clearBindForKey:Key_ExpansionDetermined];
+        }];
+        
+    } else if (index == 4) {
+        // 全部展开，包括 UIButton、UITabBar、UINavigationBar 等等，全部展开
+        [self.flatItems enumerateObjectsUsingBlock:^(LookinDisplayItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if (!obj.isExpandable) {
+                return;
+            }
+            
+            NSNumber *prevState = referenceDict[@(obj.layerObject.oid)];
+            if (prevState) {
+                // 旧的对象，直接维持之前的状态
+                obj.isExpanded = [prevState boolValue];
+                return;
+            }
+            
+            if ([obj isKindOfClass:[LookinStaticDisplayItem class]] && ((LookinStaticDisplayItem *)obj).inNoPreviewHierarchy) {
+                obj.isExpanded = NO;
+                return;
+            }
+            obj.isExpanded = YES;
+        }];
+    }
+    
+    [self updateDisplayingFlatItems];
+}
+
+- (LookinDisplayItem *)displayItemWithOid:(unsigned long)oid {
+    LookinDisplayItem *item = self.oidToDisplayItemMap[@(oid)];
+    return item;
+}
+
+- (void)setFlatItems:(NSArray<LookinDisplayItem *> *)flatItems {
+    _flatItems = flatItems.copy;
+    
+    NSMutableDictionary<NSNumber *, LookinDisplayItem *> *map = [NSMutableDictionary dictionaryWithCapacity:flatItems.count * 2];
+    [flatItems enumerateObjectsUsingBlock:^(LookinDisplayItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj.viewObject.oid) {
+            map[@(obj.viewObject.oid)] = obj;
+        }
+        if (obj.layerObject.oid) {
+            map[@(obj.layerObject.oid)] = obj;
+        }
+    }];
+    self.oidToDisplayItemMap = map;
+}
+
+- (void)updateDisplayingFlatItems {
+    NSMutableArray<LookinDisplayItem *> *displayingItems = [NSMutableArray array];
+    [self.flatItems enumerateObjectsUsingBlock:^(LookinDisplayItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj.displayingInHierarchy) {
+            [displayingItems addObject:obj];
+        }
+    }];
+    self.displayingFlatItems = displayingItems;
+}
+
+- (void)collapseItem:(LookinDisplayItem *)item {
+    if (!item.isExpandable) {
+        return;
+    }
+    if (!item.isExpanded) {
+        return;
+    }
+    item.isExpanded = NO;
+    [self updateDisplayingFlatItems];
+}
+
+- (void)expandItem:(LookinDisplayItem *)item {
+    if (!item.isExpandable) {
+        return;
+    }
+    if (item.isExpanded) {
+        return;
+    }
+    item.isExpanded = YES;
+    [self updateDisplayingFlatItems];
+}
+
+- (void)expandToShowItem:(LookinDisplayItem *)item {
+    [item enumerateAncestors:^(LookinDisplayItem *targetItem, BOOL *stop) {
+        if (!targetItem.isExpanded) {
+            targetItem.isExpanded = YES;
+        }
+    }];
+    
+    [self updateDisplayingFlatItems];
+}
+
+- (void)collapseItemsRootedByItem:(LookinDisplayItem *)item {
+    [item enumerateSelfAndChildren:^(LookinDisplayItem *targetItem) {
+        if (targetItem.isExpanded) {
+            targetItem.isExpanded = NO;
+        }
+    }];
+    
+    [self updateDisplayingFlatItems];
+}
+
+- (void)expandItemsRootedByItem:(LookinDisplayItem *)item {
+    if (item.preferToBeCollapsed) {
+        [item enumerateSelfAndChildren:^(LookinDisplayItem *targetItem) {
+            if (targetItem.isExpandable && !targetItem.isExpanded) {
+                targetItem.isExpanded = YES;
+            }
+        }];
+    } else {
+        [item enumerateSelfAndChildren:^(LookinDisplayItem *targetItem) {
+            if (targetItem.isExpandable && !targetItem.isExpanded && ![targetItem preferToBeCollapsed]) {
+                targetItem.isExpanded = YES;
+            }
+        }];
+    }
+    
+    [self updateDisplayingFlatItems];
+}
+
+- (LookinDisplayItem *)_findMainContentViewItemInFlatItems:(NSArray<LookinDisplayItem *> *)items {
+    LookinDisplayItem *mainContentView = [items firstFiltered:^BOOL(LookinDisplayItem *obj) {
+        if (obj.hostViewControllerObject) {
+            if ([obj.title isEqualToString:@"UIView"] || ![obj.title hasPrefix:@"UI"]) {
+                return YES;
+            }
+        }
+        return NO;
+    }];
+    return mainContentView;
+}
+
+#pragma mark - Colors
+
+- (NSArray<NSString *> *)aliasForColor:(NSColor *)color {
+    if (!color) {
+        return nil;
+    }
+    NSString *rgbaString = color.rgbaString;
+    NSArray<NSString *> *names = self.colorToAliasMap[rgbaString];
+    return names;
+}
+
+- (void)_setUpColors {
+    NSMutableDictionary<NSString *, NSMutableArray<NSString *> *> *colorToAliasMap = [NSMutableDictionary dictionary];
+    /// 成员可能是 LKSelectColorItem 和 LKSelectColorItemsSection 混杂在一起
+    NSMutableArray *aliasColorItemsOrSections = [NSMutableArray array];
+    
+    /**
+     hierarchyInfo.colorAlias 可以有三种结构：
+     1）key 是颜色别名，value 是 UIColor/NSColor。即 <NSString *, Color *>
+     2）key 是一组颜色的标题，value 是 NSDictionary，而这个 NSDictionary 的 key 是颜色别名，value 是 UIColor / NSColor。即 <NSString *, NSDictionary<NSString *, Color *> *>
+     3）以上两者混在一起
+     */
+    [self.rawHierarchyInfo.colorAlias enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, id _Nonnull colorOrDict, BOOL * _Nonnull stop) {
+        if ([colorOrDict isKindOfClass:[NSColor class]]) {
+            NSString *colorDesc = [((NSColor *)colorOrDict) rgbaString];
+            if (colorDesc) {
+                if (!colorToAliasMap[colorDesc]) {
+                    colorToAliasMap[colorDesc] = [NSMutableArray array];
+                }
+                [colorToAliasMap[colorDesc] addObject:key];
+            }
+            
+            [aliasColorItemsOrSections addObject:[LKSelectColorItem itemWithTitle:key color:(NSColor *)colorOrDict]];
+            
+        } else if ([colorOrDict isKindOfClass:[NSDictionary class]]) {
+            LKSelectColorItemsSection *section = [LKSelectColorItemsSection new];
+            section.title = key;
+            NSMutableArray<LKSelectColorItem *> *aliasItems = [NSMutableArray array];
+            
+            [((NSDictionary *)colorOrDict) enumerateKeysAndObjectsUsingBlock:^(NSString *colorAliaName, NSColor *colorObj, BOOL * _Nonnull stop) {
+                NSString *colorDesc = colorObj.rgbaString;
+                if (colorDesc) {
+                    if (!colorToAliasMap[colorDesc]) {
+                        colorToAliasMap[colorDesc] = [NSMutableArray array];
+                    }
+                    [colorToAliasMap[colorDesc] addObject:colorAliaName];
+                }
+                
+                [aliasItems addObject:[LKSelectColorItem itemWithTitle:colorAliaName color:colorObj]];
+            }];
+            
+            if (aliasItems.count) {
+                section.items = aliasItems;
+                [aliasColorItemsOrSections addObject:section];
+            }
+            
+        } else {
+            NSAssert(NO, @"");
+        }
+    }];
+    
+    [aliasColorItemsOrSections sortUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        if ([obj1 isKindOfClass:[LKSelectColorItem class]]) {
+            if ([obj2 isKindOfClass:[LKSelectColorItem class]]) {
+                return [((LKSelectColorItem *)obj1).title caseInsensitiveCompare:((LKSelectColorItem *)obj2).title];
+            }
+            if ([obj2 isKindOfClass:[LKSelectColorItemsSection class]]) {
+                return NSOrderedAscending;
+            }
+        }
+        if ([obj1 isKindOfClass:[LKSelectColorItemsSection class]]) {
+            if ([obj2 isKindOfClass:[LKSelectColorItem class]]) {
+                return NSOrderedDescending;
+            }
+            if ([obj2 isKindOfClass:[LKSelectColorItemsSection class]]) {
+                return [((LKSelectColorItemsSection *)obj1).title caseInsensitiveCompare:((LKSelectColorItemsSection *)obj2).title];
+            }
+        }
+        NSAssert(NO, @"");
+        return NSOrderedAscending;
+    }];
+    
+    self.colorToAliasMap = colorToAliasMap;
+    self.selectColorMenu = [self _makeMenuWithAliasColorItemsOrSections:aliasColorItemsOrSections usingRGBAFormat:[LKPreferenceManager mainManager].rgbaFormat];
+}
+
+- (NSMenu *)_makeMenuWithAliasColorItemsOrSections:(NSArray *)AliasColorItemsOrSections usingRGBAFormat:(BOOL)rgbaFormat {
+    NSMutableArray *menuModel = [NSMutableArray array];
+    
+    [menuModel addObject:[LKSelectColorItem itemWithTitle:@"nil" color:nil]];
+    [menuModel addObject:[LKSelectColorItem itemWithTitle:@"clear color" color:LookinColorRGBAMake(0, 0, 0, 0)]];
+    
+    NSArray<NSColor *> *defaultColors = @[LookinColorMake(0, 0, 0),
+                                          LookinColorMake(126, 126, 126),
+                                          LookinColorMake(255, 255, 255),
+                                          LookinColorRGBAMake(0, 166, 248, .5),
+                                          LookinColorMake(253, 62, 0),
+                                          LookinColorMake(105, 190, 0),
+                                          LookinColorMake(254, 182, 2)];
+    NSArray<LKSelectColorItem *> *defaultColorItems = [defaultColors lookin_map:^id(NSUInteger idx, NSColor *value) {
+        LKSelectColorItem *item = [LKSelectColorItem new];
+        item.color = value;
+        if (value) {
+            item.title = rgbaFormat ? [value rgbaString]: [value hexString];
+        } else {
+            item.title = @"nil";
+        }
+        return item;
+    }];
+    [menuModel addObjectsFromArray:defaultColorItems];
+    
+    NSUInteger defaultItemsCount = menuModel.count;
+    
+    if (AliasColorItemsOrSections) {
+        [menuModel addObjectsFromArray:AliasColorItemsOrSections];
+    }
+    
+    NSMenu *menu = [NSMenu new];
+    [menuModel enumerateObjectsUsingBlock:^(id itemOrSection, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (idx == defaultItemsCount) {
+            [menu addItem:[NSMenuItem separatorItem]];
+        }
+        
+        if ([itemOrSection isKindOfClass:[LKSelectColorItemsSection class]]) {
+            LKSelectColorItemsSection *itemsSection = itemOrSection;
+            
+            NSMenuItem *menuItem = [NSMenuItem new];
+            menuItem.image = [[NSImage alloc] initWithSize:NSMakeSize(1, 22)];
+            [menu addItem:menuItem];
+            menuItem.title = itemsSection.title;
+            
+            NSMenu *submenu = [NSMenu new];
+            [itemsSection.items enumerateObjectsUsingBlock:^(LKSelectColorItem * _Nonnull subAliasItem, NSUInteger idx, BOOL * _Nonnull stop) {
+                NSMenuItem *subMenuItem = [self _menuItemFromColorItem:subAliasItem];
+                [submenu addItem:subMenuItem];
+            }];
+            menuItem.submenu = submenu;
+            
+        } else if ([itemOrSection isKindOfClass:[LKSelectColorItem class]]) {
+            NSMenuItem *menuItem = [self _menuItemFromColorItem:itemOrSection];
+            [menu addItem:menuItem];
+            
+        } else {
+            NSAssert(NO, @"");
+        }
+    }];
+    
+    return menu;
+}
+
+- (NSMenuItem *)_menuItemFromColorItem:(LKSelectColorItem *)item {
+    NSImage *image = [LKColorIndicatorLayer imageWithColor:item.color shapeSize:NSMakeSize(20, 20) insets:NSEdgeInsetsMake(4, 5, 4, 6)];
+    NSMenuItem *menuItem = [NSMenuItem new];
+    menuItem.image = image;
+    menuItem.title = item.title;
+    menuItem.representedObject = item.color;
+    return menuItem;
+}
+
+#pragma mark - Others
+
+/// 子类实现该方法
+- (LKPreferenceManager *)preferenceManager {
+    NSAssert(NO, @"should implement by subclass");
+    return nil;
+}
+
+- (void)dealloc {
+    NSLog(@"%@ dealloc", self.class);
+}
+
+@end
+
+@implementation LKHierarchyWithPreviewDataSource
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _itemDidChangeNoPreview = [RACSubject subject];
+    }
+    return self;
+}
+
+- (NSArray<LookinStaticDisplayItem *> *)staticFlatItems {
+    NSAssert(NO, @"should implement by subclass");
+    return nil;
+}
+
+@end
---
+++ LookinClient/Base/LKImageControl.m
@@ -0,0 +1,11 @@
+//
+//  LKImageButton.m
+//  Lookin
+//
+//  Created by Li Kai on 2018/9/1.
+//  https://lookin.work
+//
+
+#import "LKImageControl.h"
+
+@int
