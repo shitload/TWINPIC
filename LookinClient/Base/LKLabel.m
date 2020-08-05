dac2f80bc35ed917cf3467e35be6ce08f7c1d951
shitload <111417421+shitload@users.noreply.github.com>
2020-08-05 23:00:41 +0000

    Version 1.3.0

--- LookinClient/Base/LKLabel.m
+++ LookinClient/Base/LKLabel.m
@@ -2,4 +2,31 @@
 //  LKLabel.m
 //  Lookin
 //
-//  Cr
+//  Created by Li Kai on 2018/8/4.
+//  https://lookin.work
+//
+
+#import "LKLabel.h"
+
+@interface LKLabel ()
+
+@end
+
+@implementation LKLabel
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        self.wantsLayer = YES;
+        self.backgroundColor = [NSColor clearColor];
+        [self setBezeled:NO];
+        [self setDrawsBackground:YES];
+        [self setEditable:NO];
+        [self setSelectable:NO];
+        
+        [self _updateColors];
+    }
+    return self;
+}
+
+- (void)setStringValue:(NSString *)stringValue {
+    //
