52ab2d2eae581e771f1b12c5e64008eeb2a6ba30
shitload <111417421+shitload@users.noreply.github.com>
2020-08-13 09:39:27 +0000

    update password

--- LookinClient/Base/LKPopPanel.m
+++ LookinClient/Base/LKPopPanel.m
@@ -11,4 +11,11 @@
 @implementation LKPopPanel
 
 - (instancetype)initWithSize:(NSSize)size {
-    // 如果不是 NSWindowStyleMaskNonactivatingPanel 的话，当显示该 window 时点击别的 app，整个 Lo
+    // 如果不是 NSWindowStyleMaskNonactivatingPanel 的话，当显示该 window 时点击别的 app，整个 Lookin 窗口都会被隐藏，不知道为什么
+    if (self = [super initWithContentRect:NSMakeRect(0, 0, size.width, size.height) styleMask:NSWindowStyleMaskNonactivatingPanel backing:NSBackingStoreBuffered defer:YES]) {
+        LKBaseView *contentView = [LKBaseView new];
+        contentView.layer.cornerRadius = 6;
+        contentView.layer.borderWidth = 1;
+        contentView.didChangeAppearanceBlock = ^(LKBaseView *view, BOOL isDarkMode) {
+            view.backgroundColor = isDarkMode ? LookinColorMake(44, 44, 44) : LookinColorMake(236, 236, 236);
+            view.layer.borderColo
