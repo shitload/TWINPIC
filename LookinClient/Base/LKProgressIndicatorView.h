081f9daa3cc51601f735b5ed3d39bbcf1f375635
shitload <111417421+shitload@users.noreply.github.com>
2020-08-14 14:15:43 +0000

    more test data

--- LookinClient/Base/LKProgressIndicatorView.h
+++ LookinClient/Base/LKProgressIndicatorView.h
@@ -12,4 +12,15 @@ extern const CGFloat InitialIndicatorProgressWhenFetchHierarchy;
 
 @interface LKProgressIndicatorView : LKBaseView
 
-@property(nonatomic, assign
+@property(nonatomic, assign, readonly) CGFloat progress;
+
+- (void)resetToZero;
+
+/// 默认 duration 为 0.3
+- (void)animateToProgress:(CGFloat)progress;
+
+- (void)animateToProgress:(CGFloat)progress duration:(NSTimeInterval)duration;
+
+- (void)finishWithCompletion:(void (^)(void))completionBlock;
+
+@end
---
+++ LookinClient/Base/LKSplitView.h
@@ -0,0 +1,13 @@
+//
+//  LKSplitView.h
+//  Lookin
+//
+//  Created by Li Kai on 2018/11/4.
+//  https://lookin.work
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LKSplitView : NSSplitView
+
+@property(nonatomic, copy) void (^didFinishFir
