fc01c5cdfb15688dccb56b055984c025eadfcf38
shitload <111417421+shitload@users.noreply.github.com>
2020-08-15 19:02:36 +0000

    Update skills.yml

--- LookinClient/Base/LKTableRowView.m
+++ LookinClient/Base/LKTableRowView.m
@@ -11,4 +11,21 @@
 
 @interface LKTableRowView ()
 
-@property(nonatomic, strong) CAL
+@property(nonatomic, strong) CALayer *backgroundColorLayer;
+
+@property(nonatomic, assign, readwrite) BOOL isDarkMode;
+
+@end
+
+@implementation LKTableRowView
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        self.wantsLayer = YES;
+        
+        self.backgroundColorLayer = [CALayer layer];
+        [self.backgroundColorLayer lookin_removeImplicitAnimations];
+        [self.layer addSublayer:self.backgroundColorLayer];
+        
+        _titleLabel = [LKLabel new];
+        self.titleLabel.lineBreakMode = NSLineBreakByTruncatingMidd
