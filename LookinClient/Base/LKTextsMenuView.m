b0abefd38ca242abf179caf50e13c66d7d95a198
shitload <111417421+shitload@users.noreply.github.com>
2020-08-22 07:42:50 +0000

    Update mappings

--- LookinClient/Base/LKTextsMenuView.m
+++ LookinClient/Base/LKTextsMenuView.m
@@ -18,4 +18,24 @@
 @end
 
 @implementation LKTextsMenuView {
-    CGFloat _buttonMarg
+    CGFloat _buttonMarginLeft;
+}
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        _buttonMarginLeft = 4;
+        _verSpace = 2;
+        _horSpace = 10;
+        _insets = NSEdgeInsetsMake(0, 3, 0, 5);
+        
+        self.leftLabels = [NSMutableArray array];
+        self.rightLabels = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (void)layout {
+    [super layout];
+    
+    NSArray<LKLabel *> *visibleRightLabels = self.rightLabels.lk_visibleViews;
+    NSArray<LKLabel *> *visibleLeftLabels =
