b4ef8612dc140f5ff936121a47f27c9023cf7700
shitload <111417421+shitload@users.noreply.github.com>
2020-08-26 15:49:28 +0000

    Documentation++

--- LookinClient/Base/LKTextsMenuView.m
+++ LookinClient/Base/LKTextsMenuView.m
@@ -100,4 +100,19 @@
         obj.hidden = YES;
     } doNext:^(NSUInteger idx, LKLabel *obj) {
         obj.hidden = NO;
-        obj.stringValue = texts
+        obj.stringValue = texts[idx].first;
+    }];
+
+    [self.rightLabels lookin_dequeueWithCount:texts.count add:^LKLabel *(NSUInteger idx) {
+        LKLabel *label = [LKLabel new];
+        label.selectable = YES;
+        label.font = self.font;
+        label.maximumNumberOfLines = 1;
+        label.lineBreakMode = NSLineBreakByTruncatingMiddle;
+        [self addSubview:label];
+        return label;
+    } notDequeued:^(NSUInteger idx, LKLabel *obj) {
+        obj.hidden = YES;
+    } doNext:^(NSUInteger idx, LKLabel *obj) {
+        obj.hidden = NO;
+        obj.stringValue =
