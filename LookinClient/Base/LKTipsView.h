f79da15f107276365db8afcd4e8864ef46dc43c2
shitload <111417421+shitload@users.noreply.github.com>
2020-09-05 05:46:42 +0000

    link test

--- LookinClient/Base/LKTipsView.h
+++ LookinClient/Base/LKTipsView.h
@@ -15,4 +15,25 @@
 
 @property(nonatomic, copy) NSString *title;
 
-/// 请外部不要直接修改 button 的 text 等属性，而是要通过
+/// 请外部不要直接修改 button 的 text 等属性，而是要通过下面的 buttonText 和 buttonImage 来设置
+@property(nonatomic, strong, readonly) NSButton *button;
+
+/// buttonText 和 buttonImage 只有一个会生效，请勿同时设置
+@property(nonatomic, copy) NSString *buttonText;
+@property(nonatomic, strong) NSImage *buttonImage;
+
+@property(nonatomic, strong) NSImage *image;
+
+- (void)setImageByDeviceType:(LookinAppInfoDevice)type;
+
+@property(nonatomic, weak) id target;
+
+@property(nonatomic, assign) SEL clickAction;
+@property(nonatomic, copy) void (^didClick)(LKTipsView *tipsView);
+
+@end
+
+@interface LKRedTipsView : LKTipsView
+
+- (void)startAnimation;
+- (v
