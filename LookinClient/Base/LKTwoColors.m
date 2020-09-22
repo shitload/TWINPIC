1a5cf74f321c2d4522683b92b42f9cb1c328a40b
shitload <111417421+shitload@users.noreply.github.com>
2020-09-22 19:55:35 +0000

    problem

---
+++ LookinClient/Base/LKTwoColors.m
@@ -0,0 +1,13 @@
+//
+//  LKTwoColors.m
+//  Lookin
+//
+//  Created by Li Kai on 2019/9/30.
+//  https://lookin.work
+//
+
+#import "LKTwoColors.h"
+
+@implementation LKTwoColors
+
++ (instancetype)color
