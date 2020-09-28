8f6b53f73ca777219abcb318af22fd18e5e3961e
shitload <111417421+shitload@users.noreply.github.com>
2020-09-28 12:54:09 +0000

    Update init.pp

--- LookinClient/Base/LKUserActionManager.h
+++ LookinClient/Base/LKUserActionManager.h
@@ -6,4 +6,22 @@
 //  https://lookin.work
 //
 
-#imp
+#import <Foundation/Foundation.h>
+
+@class LKUserActionManager;
+
+typedef NS_ENUM(NSInteger, LKUserActionType) {
+    LKUserActionType_None,
+    LKUserActionType_PreviewOperation,  // 在 preview 里执行了 click、double click、pan 之类的操作
+    LKUserActionType_DashboardClick,    // 点击了 dashboard
+    LKUserActionType_SelectedItemChange,    // selectedItem 改变了
+};
+
+@protocol LKUserActionManagerDelegate <NSObject>
+
+/// 当 sendAction 被业务调用时，该 delegate 方法也会被调用
+- (void)LKUserActionManager:(LKUserActionManager *)manager didAct:(LKUserActionType)type;
+
+@end
+
+@interface LKUserActionManager :
