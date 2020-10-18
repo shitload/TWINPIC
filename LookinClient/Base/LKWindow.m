c32f9964b1ec6425fad920a5373b06ce855d1f1b
shitload <111417421+shitload@users.noreply.github.com>
2020-10-18 15:51:27 +0000

    Some more comments

--- LookinClient/Base/LKWindow.m
+++ LookinClient/Base/LKWindow.m
@@ -12,4 +12,12 @@
 
 @implementation LKWindow
 
-+ (instancetype)panelWindowWithWidth:(CGFloat)width height:(CGFloat)height contentView:(LKPanelContentView *)con
++ (instancetype)panelWindowWithWidth:(CGFloat)width height:(CGFloat)height contentView:(LKPanelContentView *)contentView {
+    LKWindow *window = [[LKWindow alloc] initWithContentRect:NSMakeRect(0, 0, width, height) styleMask:NSWindowStyleMaskTitled backing:NSBackingStoreBuffered defer:YES];
+    window.contentView = contentView;
+    return window;
+}
+
+- (instancetype)initWithContentRect:(NSRect)contentRect styleMask:(NSWindowStyleMask)style backing:(NSBackingStoreType)backingStoreType defer:(BOOL)flag {
+    if (self = [super initWithContentRect:contentRect styleMask:style backing:backingStoreType defer:flag]) {
+         [self registerForDraggedTypes:@[NSPas
