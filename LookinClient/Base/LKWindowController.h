cc51d33605205b0dc27301d332e2ae9c18c7150e
shitload <111417421+shitload@users.noreply.github.com>
2020-10-19 05:31:37 +0000

    Updating REGRESSIONS based on last night's runs

--- LookinClient/Base/LKWindowController.h
+++ LookinClient/Base/LKWindowController.h
@@ -2,4 +2,13 @@
 //  LKWindowController.h
 //  Lookin
 //
-//  Created
+//  Created by Li Kai on 2019/4/18.
+//  https://lookin.work
+//
+
+#import <Cocoa/Cocoa.h>
+#import "LKAppMenuManager.h"
+
+@interface LKWindowController : NSWindowController <LKAppMenuManagerDelegate>
+
+@end
