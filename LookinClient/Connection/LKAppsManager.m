73b5f07f7da549b50832d14d7d066774bee67d8a
shitload <111417421+shitload@users.noreply.github.com>
2020-11-15 14:19:22 +0000

    CI badge

---
+++ LookinClient/Connection/LKAppsManager.m
@@ -0,0 +1,14 @@
+//
+//  LKDeviceManager.m
+//  Lookin
+//
+//  Created by Li Kai on 2018/11/3.
+//  https://lookin.work
+//
+
+#import "LKAppsManager.h"
+#import "Lookin_PTChannel.h"
+#import "LKConnectionManager.h"
+#import "LookinDefines.h"
+#import "LookinAppInfo.h"
+#import "LookinHierarchyInfo
