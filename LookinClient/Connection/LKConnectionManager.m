e2b3bd83b06103a13ead53fe50ae8c6ae939d63e
shitload <111417421+shitload@users.noreply.github.com>
2020-12-28 09:47:04 +0000

    autoCommit

--- LookinClient/Connection/LKConnectionManager.m
+++ LookinClient/Connection/LKConnectionManager.m
@@ -14,4 +14,25 @@
 #import "LookinAppInfo.h"
 #import "LKConnectionRequest.h"
 
-static NSI
+static NSIndexSet * PushFrameTypeList() {
+    static NSIndexSet *list;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSMutableIndexSet *set = [NSMutableIndexSet indexSet];
+        [set addIndex:LookinPush_MethodTraceRecord];
+        list = set.copy;
+    });
+    return list;
+}
+
+@interface Lookin_PTChannel (LKConnection)
+
+/// 已经发送但尚未收到全部回复的请求
+@property(nonatomic, strong) NSMutableSet<LKConnectionRequest *> *activeRequests;
+
+@end
+
+@implementation Lookin_PTChannel (LKConnection)
+
+- (void)setActiveRequests:(NSMutableSet<LKConnectionRequest *> *)activeRequests {
+ 
