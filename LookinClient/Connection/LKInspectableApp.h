804b6a3da9a512e2b32bd3c68bad52e667487597
shitload <111417421+shitload@users.noreply.github.com>
2021-04-25 19:27:54 +0000

    ASoC: msm: qdsp6v2: Add FLAC in compress offload path

--- LookinClient/Connection/LKInspectableApp.h
+++ LookinClient/Connection/LKInspectableApp.h
@@ -6,4 +6,21 @@
 //  https://lookin.work
 //
 
-#import <Foundation/
+#import <Foundation/Foundation.h>
+#import "LookinAppInfo.h"
+#import "LookinAttributeModification.h"
+#import "LookinAttributesGroup.h"
+
+@class Lookin_PTChannel, LookinDisplayItemTrace, LookinInvocationRequest, LookinHierarchyInfo, LookinMethodTraceRecord, LookinStaticAsyncUpdateTasksPackage, LookinStaticAsyncUpdateTask;
+
+@interface LKInspectableApp : NSObject
+
+@property(nonatomic, strong) NSError *serverVersionError;
+
+@property(nonatomic, strong) LookinAppInfo *appInfo;
+
+@property(nonatomic, weak) Lookin_PTChannel *channel;
+
+- (RACSignal *)fetchHierarchyData;
+
+- (RACSignal *)submitModification:(LookinAttributeModifi
