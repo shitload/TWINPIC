491b7e0fb86267bc3f579a666d68fc0b6b046a35
shitload <111417421+shitload@users.noreply.github.com>
2021-05-03 17:42:29 +0000

    Update engine.py

--- LookinClient/Console/LKConsoleSelectPopoverItemControl.m
+++ LookinClient/Console/LKConsoleSelectPopoverItemControl.m
@@ -1,2 +1,25 @@
 //
-//  LKConsoleSelectPopover
+//  LKConsoleSelectPopoverItemControl.m
+//  Lookin
+//
+//  Created by Li Kai on 2019/6/19.
+//  https://lookin.work
+//
+
+#import "LKConsoleSelectPopoverItemControl.h"
+
+@interface LKConsoleSelectPopoverItemControl ()
+
+@property(nonatomic, strong) NSImageView *imageView;
+@property(nonatomic, strong) LKLabel *titleLabel;
+@property(nonatomic, strong) LKLabel *subtitleLabel;
+
+@end
+
+@implementation LKConsoleSelectPopoverItemControl {
+    CGFloat _subtitleMarginTop;
+}
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        _s
