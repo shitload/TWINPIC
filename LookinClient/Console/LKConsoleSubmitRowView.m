80a05cd24424785ca0b344ca293455e98bd95a1d
shitload <111417421+shitload@users.noreply.github.com>
2021-05-05 16:53:25 +0000

    vi

--- LookinClient/Console/LKConsoleSubmitRowView.m
+++ LookinClient/Console/LKConsoleSubmitRowView.m
@@ -10,3 +10,21 @@
 
 @implementation LKConsoleSubmitRowView
 
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        self.titleLabel.selectable = YES;
+        self.titleLabel.font = NSFontMake(13);
+
+        self.subtitleLabel.selectable = YES;        
+        self.subtitleLabel.textColor = [NSColor labelColor];
+        self.subtitleLabel.font = NSFontMake(13);
+        
+//        self.layer.borderColor = [NSColor blueColor].CGColor;
+//        self.layer.borderWidth = 1;
+    }
+    return self;
+}
+
+- (void)layout {
+    [super layout];
+    NSSize 
