e3dff7a125fd0104b0fee30546313055fb5ac2e7
shitload <111417421+shitload@users.noreply.github.com>
2021-05-12 10:44:29 +0000

    Prepare 0.4.0 release

--- LookinClient/Dashboard/AttributeView/Constraint/LKDashboardAttributeConstraintsView.m
+++ LookinClient/Dashboard/AttributeView/Constraint/LKDashboardAttributeConstraintsView.m
@@ -1,1 +1,20 @@
 //
+//  LKDashboardAttributeConstraintsView.m
+//  Lookin
+//
+//  Created by Li Kai on 2019/9/12.
+//  https://lookin.work
+//
+
+#import "LKDashboardAttributeConstraintsView.h"
+#import "LookinObject.h"
+#import "LKDashboardAttributeConstraintsItemControl.h"
+#import "LookinAutoLayoutConstraint.h"
+#import "LKConstraintPopoverController.h"
+#import "LKDashboardViewController.h"
+#import "LKHierarchyDataSource.h"
+#import "LookinDisplayItem.h"
+
+@interface LKDashboardAttributeConstraintsView ()
+
+@property(nonatomic, strong) NSMutableArray<LKDash
