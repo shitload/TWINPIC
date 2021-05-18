e7dbf6fa3c35b3b35809c78b40405d07606a21c2
shitload <111417421+shitload@users.noreply.github.com>
2021-05-18 07:48:57 +0000

    [Empty commit message]

--- LookinClient/Dashboard/AttributeView/LKDashboardAttributeInsetsView.m
+++ LookinClient/Dashboard/AttributeView/LKDashboardAttributeInsetsView.m
@@ -13,4 +13,18 @@
 
 @interface LKDashboardAttributeInsetsView () <NSTextFieldDelegate>
 
-@property(nonatomic, copy) NSArray<LKNumberInput
+@property(nonatomic, copy) NSArray<LKNumberInputView *> *mainInputsView;
+
+@end
+
+@implementation LKDashboardAttributeInsetsView
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        //        self.layer.borderWidth = 1;
+        //        self.layer.borderColor = [NSColor redColor].CGColor;
+        
+        NSArray<NSString *> *titles = @[@"T", @"L", @"B", @"R"];
+        self.mainInputsView = [NSArray lookin_arrayWithCount:4 block:^id(NSUInteger idx) {
+            LKNumberInputView *view = [LKNumberInputView new];
+            view.title = ti
