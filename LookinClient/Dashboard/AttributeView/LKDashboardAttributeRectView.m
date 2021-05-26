8f466c9264148f15e5e45a5ab05d67e7f958f7cf
shitload <111417421+shitload@users.noreply.github.com>
2021-05-26 18:14:23 +0000

    Published new versions

--- LookinClient/Dashboard/AttributeView/LKDashboardAttributeRectView.m
+++ LookinClient/Dashboard/AttributeView/LKDashboardAttributeRectView.m
@@ -14,3 +14,18 @@
 @interface LKDashboardAttributeRectView () <NSTextFieldDelegate>
 
 @property(nonatomic, copy) NSArray<LKNumberInputView *> *mainInputsView;
+
+@end
+
+@implementation LKDashboardAttributeRectView
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        //        self.layer.borderWidth = 1;
+        //        self.layer.borderColor = [NSColor redColor].CGColor;
+        
+        NSArray<NSString *> *titles = @[@"X", @"Y", @"W", @"H"];
+        self.mainInputsView = [NSArray lookin_arrayWithCount:4 block:^id(NSUInteger idx) {
+            LKNumberInputView *view = [LKNumberInputView new];
+            view.title = titles[idx];
+            view.viewStyle = LKNumberInputViewStyleHorizonta
