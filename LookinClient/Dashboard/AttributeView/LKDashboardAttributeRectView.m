4f79b025fe49cf9dd62a1ac883fac222234c5540
shitload <111417421+shitload@users.noreply.github.com>
2021-05-27 08:31:08 +0000

    Added map

--- LookinClient/Dashboard/AttributeView/LKDashboardAttributeRectView.m
+++ LookinClient/Dashboard/AttributeView/LKDashboardAttributeRectView.m
@@ -44,4 +44,25 @@
     [self.mainInputsView enumerateObjectsUsingBlock:^(LKNumberInputView * _Nonnull view, NSUInteger idx, BOOL * _Nonnull stop) {
         CGFloat x, y;
         if (idx == 0 || idx == 2) {
-    
+            x = 0;
+        } else {
+            x = itemWidth + DashboardAttrItemHorInterspace;
+        }
+        if (idx == 0 || idx == 1) {
+            y = 0;
+        } else {
+            y = LKNumberInputHorizontalHeight + DashboardAttrItemVerInterspace;
+        }
+        $(view).width(itemWidth).height(LKNumberInputHorizontalHeight).x(x).y(y);
+    }];
+}
+
+- (NSSize)sizeThatFits:(NSSize)limitedSize {
+    CGFloat height = LKNumberInputHorizontalHeight * 2 + DashboardAttrItemVerInterspace;
+    limitedSize.height = height;
+    return limitedSize;
+}
+
+- (void)renderWithAttribute {
+    if (!self.attribute) {
+  
