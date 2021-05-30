a7ff1c49b77448c47896b7e63d1c8145923e71d6
shitload <111417421+shitload@users.noreply.github.com>
2021-05-30 16:35:02 +0000

    update AUTHORS

--- LookinClient/Dashboard/AttributeView/LKDashboardAttributeRowsCountView.m
+++ LookinClient/Dashboard/AttributeView/LKDashboardAttributeRowsCountView.m
@@ -9,3 +9,26 @@
 #import "LKDashboardAttributeRowsCountView.h"
 #import "LKNumberInputView.h"
 #import "LKTextFieldView.h"
+#import "LKDashboardViewController.h"
+
+@interface LKDashboardAttributeRowsCountView ()
+
+@property(nonatomic, copy) NSArray<LKNumberInputView *> *inputsView;
+
+@end
+
+@implementation LKDashboardAttributeRowsCountView
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        //        self.layer.borderWidth = 1;
+        //        self.layer.borderColor = [NSColor redColor].CGColor;
+        
+        self.inputsView = [NSArray array];
+    }
+    return self;
+}
+
+- (void)layout {
+    [super layout];
+    [self.inputsVie
