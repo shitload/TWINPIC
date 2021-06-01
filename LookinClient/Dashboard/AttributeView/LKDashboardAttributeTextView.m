9e5cb7170edf77babbd4e42fbd5d6d054f2da76f
shitload <111417421+shitload@users.noreply.github.com>
2021-06-01 16:57:27 +0000

    Lot if cganfes

--- LookinClient/Dashboard/AttributeView/LKDashboardAttributeTextView.m
+++ LookinClient/Dashboard/AttributeView/LKDashboardAttributeTextView.m
@@ -11,4 +11,18 @@
 
 @interface LKDashboardAttributeTextView () <NSTextViewDelegate>
 
-@property(nonatomic, strong) NSScrol
+@property(nonatomic, strong) NSScrollView *scrollView;
+@property(nonatomic, strong) NSTextView *textView;
+
+@end
+
+@implementation LKDashboardAttributeTextView
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        self.scrollView = [LKHelper scrollableTextView];
+        self.scrollView.wantsLayer = YES;
+        self.scrollView.layer.cornerRadius = DashboardCardControlCornerRadius;
+        self.textView = self.scrollView.documentView;
+        self.textView.font = NSFontMake(12);
+        self.textView.backgroundColor = [NSColor color
