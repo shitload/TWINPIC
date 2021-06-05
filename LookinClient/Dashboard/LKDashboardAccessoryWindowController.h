11327f13d998c133d25596824ccf1aad02bd1d5a
shitload <111417421+shitload@users.noreply.github.com>
2021-06-05 06:28:08 +0000

    Spelling!

--- LookinClient/Dashboard/LKDashboardAccessoryWindowController.h
+++ LookinClient/Dashboard/LKDashboardAccessoryWindowController.h
@@ -2,4 +2,23 @@
 //  LKDashboardAccessoryWindowController.h
 //  Lookin
 //
-//  Created by Li Kai on 20
+//  Created by Li Kai on 2019/8/30.
+//  https://lookin.work
+//
+
+#import "LKWindowController.h"
+#import "LookinAttrIdentifiers.h"
+
+@class LookinAttributesSection, LKDashboardViewController, LKDashboardAccessoryWindowController;
+
+@protocol LKDashboardAccessoryWindowControllerDelegate <NSObject>
+
+- (void)dashboardAccessoryWindowControllerWillClose:(LKDashboardAccessoryWindowController *)controller;
+
+@end
+
+@interface LKDashboardAccessoryWindowController : LKWindowController
+
+@property(nonatomic, weak) id<LKDashboardAccessoryWindowControllerDelegate> delegate;
+
+/// contentSize 是内容所
