f8e4c4c9c7bb28b87c0f55b0697246da48d38c52
shitload <111417421+shitload@users.noreply.github.com>
2021-06-06 23:07:17 +0000

    Updated snapshots

--- LookinClient/Dashboard/LKDashboardCardView.h
+++ LookinClient/Dashboard/LKDashboardCardView.h
@@ -10,4 +10,25 @@
 #import "LookinDisplayItem.h"
 #import "LKDashboardAttributeView.h"
 
-@class 
+@class LKDashboardViewController, LKDashboardCardView, LKDashboardSectionView;
+
+@protocol LKDashboardCardViewDelegate <NSObject>
+
+- (void)dashboardCardViewNeedToggleCollapse:(LKDashboardCardView *)view;
+
+@end
+
+@interface LKDashboardCardView : LKBaseView
+
+@property(nonatomic, weak) LKDashboardViewController *dashboardViewController;
+
+@property(nonatomic, weak) id<LKDashboardCardViewDelegate> delegate;
+
+/// 用来渲染的数据，设置该属性并不会触发任何渲染之类的行为
+@property(nonatomic, strong) LookinAttributesGroup *attrGroup;
+/// 使用 attrGroup 属性来渲染
+- (void)render;
+
+@property(nonatomic, assign) BOOL isCollapsed;
+
+- (LKDas
