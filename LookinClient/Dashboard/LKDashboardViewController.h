a542bb5f565a0e061b728978089777bccf9c10fa
shitload <111417421+shitload@users.noreply.github.com>
2021-06-07 21:05:47 +0000

    [review_completed] Updating translations for i18n/po/ja_JP/upgrading/topics/keycloak/changes.ja_JP.po

--- LookinClient/Dashboard/LKDashboardViewController.h
+++ LookinClient/Dashboard/LKDashboardViewController.h
@@ -2,4 +2,22 @@
 //  LKDashboardViewController.h
 //  Lookin
 //
-//  Created by L
+//  Created by Li Kai on 2018/8/6.
+//  https://lookin.work
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class LKHierarchyDataSource, LKStaticHierarchyDataSource, LookinAttribute, LKReadHierarchyDataSource;
+
+@interface LKDashboardViewController : LKBaseViewController
+
+- (instancetype)initWithStaticDataSource:(LKStaticHierarchyDataSource *)dataSource;
+
+- (instancetype)initWithReadDataSource:(LKReadHierarchyDataSource *)dataSource;
+
+- (LKHierarchyDataSource *)currentDataSource;
+
+- (RACSignal *)modifyAttribute:(LookinAttribute *)attribute newValue:(id)newValue;
+
+/// 如果为 YES 则表示当前使用的是 StaticDataS
