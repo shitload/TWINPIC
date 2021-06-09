4158aa31a0b7802c5827d7d16d1b15b8eccc15d9
shitload <111417421+shitload@users.noreply.github.com>
2021-06-09 15:36:11 +0000

    remove not used code

--- LookinClient/Dashboard/Search/LKDashboardSearchInputView.m
+++ LookinClient/Dashboard/Search/LKDashboardSearchInputView.m
@@ -2,4 +2,26 @@
 //  LKDashboardSearchInputView.m
 //  Lookin
 //
+//  Created by Li Kai on 2019/9/5.
+//  https://lookin.work
 //
+
+#import "LKDashboardSearchInputView.h"
+#import "LKPreferenceManager.h"
+
+@interface LKDashboardSearchInputView () <NSTextFieldDelegate>
+
+@property(nonatomic, strong) NSImageView *iconImageView;
+@property(nonatomic, strong) NSTextField *textField;
+
+@end
+
+@implementation LKDashboardSearchInputView {
+    CGFloat _iconXWhenActive;
+}
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        _iconXWhenActive = 11;
+        
+        self.layer.cornerRadius = Das
