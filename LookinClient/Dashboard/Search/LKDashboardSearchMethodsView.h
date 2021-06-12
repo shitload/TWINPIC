72d36daea3ef978da3e15e5730fd6f9f32cd50cc
shitload <111417421+shitload@users.noreply.github.com>
2021-06-12 17:40:09 +0000

    Fixed variable

--- LookinClient/Dashboard/Search/LKDashboardSearchMethodsView.h
+++ LookinClient/Dashboard/Search/LKDashboardSearchMethodsView.h
@@ -1,3 +1,23 @@
 //
 //  LKDashboardSearchMethodsView.h
-//  Look
+//  Lookin
+//
+//  Created by Li Kai on 2019/9/6.
+//  https://lookin.work
+//
+
+#import "LKDashboardSearchCardView.h"
+
+@class LKDashboardSearchMethodsView;
+
+@protocol LKDashboardSearchMethodsViewDelegate <NSObject>
+
+- (void)dashboardSearchMethodsView:(LKDashboardSearchMethodsView *)view requestToInvokeMethod:(NSString *)method oid:(unsigned long)oid;
+
+@end
+
+@interface LKDashboardSearchMethodsView : LKDashboardSearchCardView
+
+@property(nonatomic, weak) id<LKDashboardSearchMethodsViewDelegate> delegate;
+
+- (void)renderWithMethods:(NSArray<NSString *> *)methods oid:(unsigned long)oid
