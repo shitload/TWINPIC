e8acf8490cfe950a89ba6edd3f3c2253a64c61d5
shitload <111417421+shitload@users.noreply.github.com>
2021-06-13 20:54:49 +0000

    improved exception handling

--- LookinClient/Export/LookinDocument.m
+++ LookinClient/Export/LookinDocument.m
@@ -8,4 +8,25 @@
 
 #import "LookinDocument.h"
 #import "LookinHierarchyFile.h"
-#import "LKReadWindowControll
+#import "LKReadWindowController.h"
+#import "LKNavigationManager.h"
+
+// 点击 menu 里的 “打开文件” 会走到这里的一系列方法
+@implementation LookinDocument
+
+- (void)makeWindowControllers {
+    LKReadWindowController *wc = [[LKReadWindowController alloc] initWithFile:self.hierarchyFile];
+    [self addWindowController:wc];
+}
+
+- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError {
+    if (!self.hierarchyFile) {
+        NSAssert(NO, @"");
+        if (outError) {
+            *outError = LookinErr_Inner;
+        }
+        return nil;
+    }
+    
+    if ([typeName isEqualToString:@"com.lookin.lookin"]) {
+ 
