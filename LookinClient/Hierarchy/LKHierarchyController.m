94a0747005f170261b8f9229357e40683dd03233
shitload <111417421+shitload@users.noreply.github.com>
2021-06-15 15:26:58 +0000

    updated PDF version

--- LookinClient/Hierarchy/LKHierarchyController.m
+++ LookinClient/Hierarchy/LKHierarchyController.m
@@ -9,4 +9,24 @@
 #import "LKHierarchyController.h"
 #import "LKHierarchyDataSource.h"
 #import "LookinDisplayItem.h"
-#impo
+#import "LKTableView.h"
+#import "LKTutorialManager.h"
+
+@interface LKHierarchyController ()
+
+@end
+
+@implementation LKHierarchyController
+
+
+- (instancetype)initWithDataSource:(LKHierarchyDataSource *)dataSource {
+    if (self = [self initWithContainerView:nil]) {
+        _dataSource = dataSource;
+        
+        @weakify(self);
+        [RACObserve(dataSource, selectedItem) subscribeNext:^(LookinDisplayItem * _Nullable item) {
+            @strongify(self);
+            [self.hierarchyView scrollToMakeItemVisible:item];
+        }];
+        
+        RAC(self.hierarchy
