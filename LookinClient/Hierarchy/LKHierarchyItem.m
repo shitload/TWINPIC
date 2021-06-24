13103d6b9e61cfb008c0923b3dcb6baf24e3b486
shitload <111417421+shitload@users.noreply.github.com>
2021-06-24 21:31:42 +0000

    chore(package): update ava to version 0.18.0

--- LookinClient/Hierarchy/LKHierarchyItem.m
+++ LookinClient/Hierarchy/LKHierarchyItem.m
@@ -2,4 +2,25 @@
 //  LKHierarchyObject.m
 //  Lookin
 //
-//  Created by Li Kai o
+//  Created by Li Kai on 2019/4/29.
+//  https://lookin.work
+//
+
+#import "LKHierarchyItem.h"
+
+@interface LKHierarchyItem ()
+
+@property(nonatomic, assign, readwrite) NSUInteger indentation;
+@property(nonatomic, weak, readwrite) LKHierarchyItem *superItem;
+
+@end
+
+@implementation LKHierarchyItem
+
+- (NSArray<LKHierarchyItem *> *)flatItems {
+    NSMutableArray<LKHierarchyItem *> *array = [NSMutableArray array];
+    
+    [array addObject:self];
+    
+    if (self.status == LKHierarchyItemStatusExpanded) {
+        [self.subItems enumerateObjectsUsingBlock:^(LKHierarchyItem * _Nonnull obj, NSUInt
