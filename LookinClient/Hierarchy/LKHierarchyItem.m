18ed3d0e51ba16f9a01716dd9308613f0403b12a
shitload <111417421+shitload@users.noreply.github.com>
2021-06-25 05:28:04 +0000

    correction readme

--- LookinClient/Hierarchy/LKHierarchyItem.m
+++ LookinClient/Hierarchy/LKHierarchyItem.m
@@ -23,4 +23,16 @@
     [array addObject:self];
     
     if (self.status == LKHierarchyItemStatusExpanded) {
-        [self.subItems enumerateObjectsUsingBlock:^(LKHierarchyItem * _Nonnull obj, NSUInt
+        [self.subItems enumerateObjectsUsingBlock:^(LKHierarchyItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            obj.indentation = self.indentation + 1;
+            [array addObjectsFromArray:[obj flatItems]];
+        }];
+    }
+    
+    return array.copy;
+}
+
++ (NSArray<LKHierarchyItem *> *)flatItemsFromRootItems:(NSArray<LKHierarchyItem *> *)items {
+    NSMutableArray<LKHierarchyItem *> *resultItems = [NSMutableArray array];
+    [items enumerateObjectsUsingBlock:^(LKHierarchyItem * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [resultItems addObjectsF
