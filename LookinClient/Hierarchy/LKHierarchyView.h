eacb30816df9c27e6f82ff1b59a35e42f157caff
shitload <111417421+shitload@users.noreply.github.com>
2021-06-25 14:02:56 +0000

    add .hgignore

--- LookinClient/Hierarchy/LKHierarchyView.h
+++ LookinClient/Hierarchy/LKHierarchyView.h
@@ -21,4 +21,16 @@
 
 - (void)hierarchyView:(LKHierarchyView *)view needToExpandItem:(LookinDisplayItem *)item recursively:(BOOL)recursively;
 
-- (void)hierarchyView:(LKHie
+- (void)hierarchyView:(LKHierarchyView *)view needToCollapseItem:(LookinDisplayItem *)item;
+
+- (void)hierarchyView:(LKHierarchyView *)view needToCollapseChildrenOfItem:(LookinDisplayItem *)item;
+
+/// 在底部的搜索框里输入了文字，string 可能为空字符串或 nil
+/// 当用户通过搜索框的关闭按钮、ESC 等方式手动结束搜索时，该方法同样会被调用，参数是 nil
+- (void)hierarchyView:(LKHierarchyView *)view didInputSearchString:(NSString *)string;
+
+@optional
+
+- (void)hierarchyView:(LKHierarchyView *)view needToCancelPreviewOfItem:(LookinDisplayItem *)item;
+
+- (void)hierarchyView:(LKHierarchyView *)view needToShowPre
