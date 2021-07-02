36c542d081ebe78045f2f25f204ec169b106e9e5
shitload <111417421+shitload@users.noreply.github.com>
2021-07-02 20:18:55 +0000

    Gemify

--- LookinClient/Manager/LKAppMenuManager.m
+++ LookinClient/Manager/LKAppMenuManager.m
@@ -7,4 +7,23 @@
 //
 
 #import "LKAppMenuManager.h"
-#import "LKNavigati
+#import "LKNavigationManager.h"
+#import "LKLaunchWindowController.h"
+#import "LKLaunchViewController.h"
+#import "LKPreviewController.h"
+#import "LKStaticWindowController.h"
+#import "LKStaticViewController.h"
+#import "LKPreferenceManager.h"
+#import "LKStaticHierarchyDataSource.h"
+#import "LKWindowController.h"
+#include <mach-o/dyld.h>
+#import <Sparkle/Sparkle.h>
+@import AppCenter;
+@import AppCenterAnalytics;
+
+static NSUInteger const kTag_About = 11;
+static NSUInteger const kTag_Preferences = 12;
+static NSUInteger const kTag_CheckUpdates = 13;
+
+static NSUInteger const kTag_Reload = 21;
+static N
