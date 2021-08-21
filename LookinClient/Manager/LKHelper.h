c4f4a495defd6d6e0511182ce2c70a36053a79a4
shitload <111417421+shitload@users.noreply.github.com>
2021-08-21 15:08:28 +0000

    chore(deps): add renovate.json

--- LookinClient/Manager/LKHelper.h
+++ LookinClient/Manager/LKHelper.h
@@ -15,4 +15,17 @@
 #define CurrentTime [[NSDate date] timeIntervalSince1970]
 #define CurrentKeyWindow [NSApplication sharedApplication].keyWindow
 
-#define NSImageMake(imageName) [NSImage imageNamed:im
+#define NSImageMake(imageName) [NSImage imageNamed:imageName]
+#define NSFontMake(fontSize) [NSFont systemFontOfSize:fontSize]
+
+#define DashboardCardValueColor LookinColorMake(223, 223, 223)
+#define DashboardCardControlBackgroundColor LookinColorMake(40, 40, 40)
+#define DashboardCardControlBorderColor LookinColorMake(87, 87, 87)
+
+#define SeparatorLightModeColor LookinColorMake(215, 215, 215)
+#define SeparatorDarkModeColor LookinColorMake(67, 67, 69)
+
+#define NSSizeMax NSMakeSize(CGFLOAT_MAX, CGFLOAT_MAX)
+
+#define NSColorBlack LookinColorMake(13, 20, 30)
+#define NSColorWhite LookinColorMake(2
