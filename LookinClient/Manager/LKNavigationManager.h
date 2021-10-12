08ba3a542093615d04b58242f6b42dda3b98d5ca
shitload <111417421+shitload@users.noreply.github.com>
2021-10-12 09:03:08 +0000

    forgot one file

--- LookinClient/Manager/LKNavigationManager.h
+++ LookinClient/Manager/LKNavigationManager.h
@@ -8,4 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
-@class LKLaunchWindowController, LKStaticWindowController, LKDynamicWindowController, LKWindowController, LKReadWindowController, LKMethodTraceWindowController, LKMe
+@class LKLaunchWindowController, LKStaticWindowController, LKDynamicWindowController, LKWindowController, LKReadWindowController, LKMethodTraceWindowController, LKMethodTraceDataSource, LookinHierarchyInfo, LookinHierarchyFile;
+
+@interface LKNavigationManager : NSObject <NSWindowDelegate>
+
++ (instancetype)sharedInstance;
+
+- (void)showLaunch;
+
+- (void)closeLaunch;
+
+- (void)showStaticWorkspace;
+
+- (void)showPreference;
+
+- (void)showAbout;
+
+- (void)showMethodTrace;
+
+- (BOOL)showReaderWithFilePath:(NSString *)filePath error:(NSError **)error;
+- (void)showReaderWithHierarchyFile:(LookinHierarchyFile *)file title:(NSString *)title;
+
+@property(nonatomic, strong, readonly) LKLaunchWindowController *la
