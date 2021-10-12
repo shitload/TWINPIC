76c56d3f4bd1436d5d486e2c270a3af9e9335bae
shitload <111417421+shitload@users.noreply.github.com>
2021-10-12 21:38:02 +0000

    patch version bump

--- LookinClient/Manager/LKNavigationManager.m
+++ LookinClient/Manager/LKNavigationManager.m
@@ -3,4 +3,26 @@
 //  Lookin
 //
 //  Created by Li Kai on 2018/11/3.
-//  h
+//  https://lookin.work
+//
+
+#import "LKNavigationManager.h"
+#import "LKLaunchWindowController.h"
+#import "LKStaticWindowController.h"
+#import "LKPreferenceWindowController.h"
+#import "LKStaticViewController.h"
+#import "LKPreviewController.h"
+#import "LKPreviewController.h"
+#import "LKAppsManager.h"
+#import "LookinHierarchyFile.h"
+#import "LKReadWindowController.h"
+#import "LKMethodTraceWindowController.h"
+#import "LKConsoleViewController.h"
+#import "LKPreferenceManager.h"
+#import "LKAboutWindowController.h"
+@import AppCenter;
+@import AppCenterAnalytics;
+
+@interface LKNavigationManager ()
+
+@p
