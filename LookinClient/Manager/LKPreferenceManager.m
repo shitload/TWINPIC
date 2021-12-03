1db5abef048c054e216cdda097ad5a571f484491
shitload <111417421+shitload@users.noreply.github.com>
2021-12-03 07:59:13 +0000

    add filtering

--- LookinClient/Manager/LKPreferenceManager.m
+++ LookinClient/Manager/LKPreferenceManager.m
@@ -14,4 +14,15 @@
 NSString *const NotificationName_DidChangeSectionShowing = @"NotificationName_DidChangeSectionShowing";
 
 NSString *const LKWindowSizeName_Dynamic = @"LKWindowSizeName_Dynamic";
-NSString *const LKWin
+NSString *const LKWindowSizeName_Static = @"LKWindowSizeName_Static";
+NSString *const LKWindowSizeName_Methods = @"LKWindowSizeName_Methods";
+
+const CGFloat LKInitialPreviewScale = 0.27;
+
+static NSString * const Key_PreviousClientVersion = @"preVer";
+static NSString * const Key_ShowOutline = @"showOutline";
+static NSString * const Key_ShowHiddenItems = @"showHiddenItems";
+static NSString * const Key_EnableReport = @"enableReport";
+static NSString * const Key_RgbaFormat = @"egbaFormat";
+static NSString * const Key_ZInterspace = @"zInterspace_v095";
+static NSString * const K
