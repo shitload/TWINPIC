bb29cfbf0655ca163cf5d3df1e9061ae22de68e5
shitload <111417421+shitload@users.noreply.github.com>
2022-04-03 12:55:13 +0000

    Pylint fixes.

--- LookinClient/Measure/LKMeasureResultLineData.h
+++ LookinClient/Measure/LKMeasureResultLineData.h
@@ -1,1 +1,25 @@
 //
+//  LKMeasureResultHorLineData.h
+//  Lookin
+//
+//  Created by Li Kai on 2019/10/24.
+//  https://lookin.work
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LKMeasureResultHorLineData : NSObject
+
++ (instancetype)dataWithStartX:(CGFloat)startX endX:(CGFloat)endX y:(CGFloat)y value:(CGFloat)value;
+
+@property(nonatomic, assign) CGFloat startX;
+@property(nonatomic, assign) CGFloat endX;
+@property(nonatomic, assign) CGFloat y;
+
+@property(nonatomic, assign) CGFloat displayValue;
+
+@end
+
+@interface LKMeasureResultVerLineData : NSObject
+
++ (instancetype)dataWithStartY:(CGFlo
