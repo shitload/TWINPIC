50db22c77bde7b057fc4e90ff2ea04b225459fb3
shitload <111417421+shitload@users.noreply.github.com>
2022-04-17 14:27:15 +0000

    Update domoticaController_install.sh

--- LookinClient/Measure/LKMeasureResultView.m
+++ LookinClient/Measure/LKMeasureResultView.m
@@ -1,2 +1,16 @@
 //
-//  LKMeasure
+//  LKMeasureResultView.m
+//  Lookin
+//
+//  Created by Li Kai on 2019/10/21.
+//  https://lookin.work
+//
+
+#import "LKMeasureResultView.h"
+#import "LKNavigationManager.h"
+#import "LKTextFieldView.h"
+#import "LKMeasureResultLineData.h"
+
+#define compare(A, B) [self _compare:A with:B]
+#define addHor(arg_array, arg_startX, arg_endX, arg_y, arg_value) [arg_array addObject:[LKMeasureResultHorLineData dataWithStartX:arg_startX endX:arg_endX y:arg_y value:arg_value]]
+#define addVer(arg_array, arg_startY, arg_endY, arg_x, arg_value) [arg_array addObject:[LKMeasureResultVerLineData dataWithStartY:
