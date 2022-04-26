a901a56bd0e17379cbc5ecf02db3c52c17af902c
shitload <111417421+shitload@users.noreply.github.com>
2022-04-26 15:21:02 +0000

    ext4, jbd2: ensure entering into panic after recording an error in superblock

--- LookinClient/Measure/LKMeasureResultView.m
+++ LookinClient/Measure/LKMeasureResultView.m
@@ -13,4 +13,20 @@
 
 #define compare(A, B) [self _compare:A with:B]
 #define addHor(arg_array, arg_startX, arg_endX, arg_y, arg_value) [arg_array addObject:[LKMeasureResultHorLineData dataWithStartX:arg_startX endX:arg_endX y:arg_y value:arg_value]]
-#define addVer(arg_array, arg_startY, arg_endY, arg_x, arg_value) [arg_array addObject:[LKMeasureResultVerLineData dataWithStartY:
+#define addVer(arg_array, arg_startY, arg_endY, arg_x, arg_value) [arg_array addObject:[LKMeasureResultVerLineData dataWithStartY:arg_startY endY:arg_endY x:arg_x value:arg_value]]
+
+typedef NS_ENUM(NSInteger, CompareResult) {
+    Bigger,
+    Same,
+    Smaller
+};
+
+@interface LKMeasureResultView ()
+
+/// mainLayer 指 selectedLayer，referLayer 指 hovered layer
+@property(nonatomic, strong) LKBaseView *contentView;
+
+@property(nonatomic, strong) NSImageView *mainImageView;
+@property(nonatomic, strong) NSImageView *referImageView;
+
+/// 如果直接把 solidLinesLayer 作为 contentView.layer 的 sublayer 则经常出现 linesLayer 被 imageView.layer 盖住的情况，貌似 imageView.layer 是懒加载的且时机不
