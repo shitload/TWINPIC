ab87e353da21e36339a65cc893e45b5e2462952d
shitload <111417421+shitload@users.noreply.github.com>
2022-05-20 10:14:49 +0000

    deleting files

--- LookinClient/MethodTrace/LKMethodTraceDataSource.h
+++ LookinClient/MethodTrace/LKMethodTraceDataSource.h
@@ -1,3 +1,24 @@
 //
 //  LKMethodTraceDataSource.h
+//  Lookin
 //
+//  Created by Li Kai on 2019/5/24.
+//  https://lookin.work
+//
+
+#import <Foundation/Foundation.h>
+
+@class LookinMethodTraceRecord;
+
+@interface LKMethodTraceDataSource : NSObject
+
+@property(nonatomic, copy, readonly) NSArray<NSString *> *allClassNames;
+
+- (RACSignal *)syncData;
+
+- (RACSignal *)fetchSelectorNamesWithClass:(NSString *)className;
+
+- (RACSignal *)addWithClassName:(NSString *)className selName:(NSString *)selName;
+- (void)deleteWithClassName:(NSString *)className selName:(NSString *)selName;
+
+- (void)handleReceivingRecord:(LookinMet
