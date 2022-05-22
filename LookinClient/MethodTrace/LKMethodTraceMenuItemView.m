fc4324e28e3130f8f8b6ed71161dc68395c8bf8c
shitload <111417421+shitload@users.noreply.github.com>
2022-05-22 22:49:41 +0000

    ENH: Version number update

--- LookinClient/MethodTrace/LKMethodTraceMenuItemView.m
+++ LookinClient/MethodTrace/LKMethodTraceMenuItemView.m
@@ -6,4 +6,23 @@
 //  https://lookin.work
 //
 
-#import "LKMethodTraceMenuItemView.
+#import "LKMethodTraceMenuItemView.h"
+
+@interface LKMethodTraceMenuItemView ()
+
+@property(nonatomic, strong) NSImageView *imageView;
+@property(nonatomic, strong) LKLabel *label;
+@property(nonatomic, strong) NSButton *deleteButton;
+
+@end
+
+@implementation LKMethodTraceMenuItemView
+
+- (instancetype)initWithFrame:(NSRect)frameRect {
+    if (self = [super initWithFrame:frameRect]) {
+        self.imageView = [NSImageView new];
+        [self addSubview:self.imageView];
+        
+        self.label = [LKLabel new];
+        self.label.font = [NSFont systemFontOfSize:12];
+        self.label.maximumN
