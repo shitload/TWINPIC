46eb661ccd871796518d2ec71fb5cb44d6bc611d
shitload <111417421+shitload@users.noreply.github.com>
2022-05-27 07:18:43 +0000

    attempt fix

--- LookinClient/MethodTrace/LKMethodTraceMenuItemView.m
+++ LookinClient/MethodTrace/LKMethodTraceMenuItemView.m
@@ -25,4 +25,17 @@
         
         self.label = [LKLabel new];
         self.label.font = [NSFont systemFontOfSize:12];
-        self.label.maximumN
+        self.label.maximumNumberOfLines = 1;
+        self.label.lineBreakMode = NSLineBreakByTruncatingMiddle;
+        [self addSubview:self.label];
+        
+        self.deleteButton = [NSButton new];
+        self.deleteButton.bezelStyle = NSBezelStyleRoundRect;
+        self.deleteButton.bordered = NO;
+        self.deleteButton.image = NSImageMake(@"icon_delete");
+        self.deleteButton.target = self;
+        self.deleteButton.action = @selector(_handleDelete);
+        self.deleteButton.hidden = YES;
+        [self addSubview:self.deleteButton];
+        
+        
