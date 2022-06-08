17fdf1bed7bc0aaf353fd385255c444ac569bf28
shitload <111417421+shitload@users.noreply.github.com>
2022-06-08 19:11:01 +0000

    Removed old test

--- LookinClient/Preference/LKPreferencePopupView.m
+++ LookinClient/Preference/LKPreferencePopupView.m
@@ -6,4 +6,24 @@
 //  https://lookin.work
 //
 
-#import "LKPreferenc
+#import "LKPreferencePopupView.h"
+
+@interface LKPreferencePopupView ()
+
+@property(nonatomic, strong) LKLabel *titleLabel;
+
+@property(nonatomic, strong) NSPopUpButton *button;
+
+@property(nonatomic, strong) LKLabel *messageLabel;
+
+@property(nonatomic, strong) NSArray<NSString *> *messages;
+
+@end
+
+@implementation LKPreferencePopupView
+
+- (instancetype)initWithTitle:(NSString *)title message:(NSString *)message options:(NSArray<NSString *> *)options {
+    NSArray<NSString *> *messages = [NSArray lookin_arrayWithCount:options.count block:^id(NSUInteger idx) {
+        return message;
+    }];
+    retu
