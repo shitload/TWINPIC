eb5cf1d618d17dd8be725e738b74907c865eba25
shitload <111417421+shitload@users.noreply.github.com>
2022-06-08 18:06:16 +0000

    6.0.6

--- LookinClient/Preference/LKPreferencePopupView.h
+++ LookinClient/Preference/LKPreferencePopupView.h
@@ -10,4 +10,15 @@
 
 @interface LKPreferencePopupView : LKBaseView
 
-- (instancetype)initWithTitle:(NSString *)title message:(NSString *)message o
+- (instancetype)initWithTitle:(NSString *)title message:(NSString *)message options:(NSArray<NSString *> *)options;
+
+- (instancetype)initWithTitle:(NSString *)title messages:(NSArray<NSString *> *)messages options:(NSArray<NSString *> *)options;
+
+@property(nonatomic, assign) NSUInteger selectedIndex;
+@property(nonatomic, copy) void (^didChange)(NSUInteger selectedIndex);
+
+@property(nonatomic, assign) BOOL isEnabled;
+
+@property(nonatomic, assign) CGFloat buttonX;
+
+@end
---
+++ LookinClient/Preference/LKPreferencePopupView.m
@@ -0,0 +1,9 @@
+//
+//  LKPreferencePopupView.m
+//  Lookin
+//
+//  Created by Li Kai on 2019/2/28.
+//  https://lookin.work
+//
+
+#import "LKPreferenc
