959d56ceb8f3ca9c7b5a42ca9cb81eb6214dca4f
shitload <111417421+shitload@users.noreply.github.com>
2022-06-09 08:43:33 +0000

    inject

--- LookinClient/Preference/LKPreferencePopupView.m
+++ LookinClient/Preference/LKPreferencePopupView.m
@@ -26,4 +26,16 @@
     NSArray<NSString *> *messages = [NSArray lookin_arrayWithCount:options.count block:^id(NSUInteger idx) {
         return message;
     }];
-    retu
+    return [self initWithTitle:title messages:messages options:options];
+}
+
+- (instancetype)initWithTitle:(NSString *)title messages:(NSArray<NSString *> *)messages options:(NSArray<NSString *> *)options {
+    if (self = [self initWithFrame:NSZeroRect]) {
+        
+        _isEnabled = YES;
+        self.messages = messages;
+        
+        self.titleLabel = [LKLabel new];
+        self.titleLabel.stringValue = title;
+        self.titleLabel.font = NSFontMake(IsEnglish ? 13 : 15);
+        [self addSubview:self.titleLabel];
