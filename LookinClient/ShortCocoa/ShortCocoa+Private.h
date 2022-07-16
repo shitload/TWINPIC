3a3589056074b7a1757bdebc62997b8b899460c8
shitload <111417421+shitload@users.noreply.github.com>
2022-07-16 19:24:45 +0000

    Sloader update on _data/VueJs.json

--- LookinClient/ShortCocoa/ShortCocoa+Private.h
+++ LookinClient/ShortCocoa/ShortCocoa+Private.h
@@ -17,4 +17,24 @@
 
 #define ShortCocoaEqualClass(object, CLASS) [object isKindOfClass:[CLASS class]]
 
-NS_ASSUM
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShortCocoa (Private)
+
+/**
+ 遍历 self.get，并将其中类型为 classA 的对象传入 handlerA 中
+ */
+- (void)unpack:(Class)classA do:(void (^)(id obj, BOOL *stop))handlerA;
+
+/**
+ 遍历 self.get，并将其中类型为 classA 的对象传入 handlerA 中，将类型为 classB 的对象传入 handlerB 中
+ */
+- (void)unpackClassA:(Class)classA doA:(void (^)(id obj, BOOL *stop))handlerA classB:(nullable Class)classB doB:(nullable void (^)(id obj, BOOL *stop))handlerB;
+
+/// 辅助进行字符串相关的处理
+@property(nonatomic, strong) NSMutableAttributedString *cachedAttrString;
+
+@end
+
+@interface ShortCocoaHelper : NSObject
+
