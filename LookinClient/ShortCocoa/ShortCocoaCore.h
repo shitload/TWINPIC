6c0e19ad36f446deb3e702e7f87a360ff1d75294
shitload <111417421+shitload@users.noreply.github.com>
2022-08-04 22:48:39 +0000

    Using latest Zephir

--- LookinClient/ShortCocoa/ShortCocoaCore.h
+++ LookinClient/ShortCocoa/ShortCocoaCore.h
@@ -54,4 +54,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  便利的 ShortCocoa 初始化方法，该参数的第一个传入值 something 可以为对象也可以为 Class，从而实现了 $(UIView) 这种写法
- @note 该方法因为有 ShortCocoa 的前缀命名空间因此更
+ @note 该方法因为有 ShortCocoa 的前缀命名空间因此更加安全，主要在 ShortCocoa 内部使用。外部请使用更便利的 $(...) 方法
+ */
+#define ShortCocoaMake(something, ...) [[ShortCocoa alloc] initWithObjectsCount:(ShortCocoaCountArgs(ShortCocoaMakeInstance([something self]), ##__VA_ARGS__)) objects:ShortCocoaMakeInstance([something self]), ##__VA_ARGS__]
+
+/**
+ 在编译时计算传入的参数数量，从而实现 $(label1, label2) 这种写法，即参数数量不确定而又无需在末尾标注 nil 等符号
+ @note 如果没有传入任何参数则返回 1
+ @warning 最多支持传入 63 个参数，并非无限
+ */
+#define ShortCocoaCountArgs(...) ShortCocoaCountArgs1(__VA_ARGS__,ShortCocoaCountArgs2())
+#define ShortCocoaCountArgs1(...) ShortCocoaCountArgs3(__VA_ARGS__)
+#define ShortCocoa
