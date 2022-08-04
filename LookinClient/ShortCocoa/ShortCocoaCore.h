3b1a8784295c79c1254031207d3b18587c34b594
shitload <111417421+shitload@users.noreply.github.com>
2022-08-04 17:42:19 +0000

    theme updates

--- LookinClient/ShortCocoa/ShortCocoaCore.h
+++ LookinClient/ShortCocoa/ShortCocoaCore.h
@@ -24,4 +24,34 @@ NS_ASSUME_NONNULL_BEGIN
  @note 不推荐使用这个繁琐的语法，请使用 $(...) 这个宏
  
  @param count 在 object 里传入的参数数量
- @param obj
+ @param object 被包装的对象
+ */
+- (instancetype)initWithObjectsCount:(uint)count objects:(nullable id)object,...;
+
+/**
+ 获取被 ShortCocoa 包装的原始对象
+ 
+ 一种常见的使用场景：
+ @code
+ self.label = $(UILabel).text(@"Hello").addTo(self.view).get;
+ @endcode
+ 
+ 各种情况的返回值：
+ @code
+ 
+ $(self.label).get; // => self.label
+ 
+ $(label1, nil, label2).get; // => @[label1, label2]
+ 
+ $(UIView).get;     // => [UIView new]，即返回一个 UIView 的实例
+ 
+ @endcode
+ 
+ */
+@property(nonatomic, strong, readonly, nullable) id get;
+
+@end
+
+/**
+ 便利的 ShortCocoa 初始化方法，该参数的第一个传入值 something 可以为对象也可以为 Class，从而实现了 $(UIView) 这种写法
+ @note 该方法因为有 ShortCocoa 的前缀命名空间因此更
