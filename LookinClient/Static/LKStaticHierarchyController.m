8285254f92fbbe2930c459dbe63203f3b7931bb1
shitload <111417421+shitload@users.noreply.github.com>
2022-08-08 07:55:54 +0000

    cifs: on CONFIG_HIGHMEM machines, limit the rsize/wsize to the kmap space

--- LookinClient/Static/LKStaticHierarchyController.m
+++ LookinClient/Static/LKStaticHierarchyController.m
@@ -1,2 +1,22 @@
 //
-//  LKHierarchyViewControlle
+//  LKHierarchyViewController.m
+//  Lookin
+//
+//  Created by Li Kai on 2018/8/4.
+//  https://lookin.work
+//
+
+#import "LKStaticHierarchyController.h"
+#import "LKStaticHierarchyDataSource.h"
+#import "LookinDisplayItem.h"
+
+@implementation LKStaticHierarchyController
+
+#pragma mark - LKHierarchyViewDelegate
+
+- (void)hierarchyView:(LKHierarchyView *)view needToCancelPreviewOfItem:(LookinDisplayItem *)item {
+    item.noPreview = YES;
+    [((LKStaticHierarchyDataSource *)self.dataSource).itemDidChangeNoPreview sendNext:nil];
+}
+
+- (void)hierarchyView:(LKHierarchyView *)view needToShowPreviewOfI
