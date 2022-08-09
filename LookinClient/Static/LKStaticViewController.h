e3d5d29c11a4d4f6f12ec09d2162032aa17847d3
shitload <111417421+shitload@users.noreply.github.com>
2022-08-09 08:02:16 +0000

    changing names

--- LookinClient/Static/LKStaticViewController.h
+++ LookinClient/Static/LKStaticViewController.h
@@ -3,4 +3,27 @@
 //  Lookin
 //
 //  Created by Li Kai on 2018/8/4.
-//  h
+//  https://lookin.work
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class LKPreviewController, LKProgressIndicatorView, LKHierarchyView;
+
+@interface LKStaticViewController : LKBaseViewController
+
+@property(nonatomic, strong, readonly) LKPreviewController *viewsPreviewController;
+
+@property(nonatomic, strong) LKProgressIndicatorView *progressView;
+
+@property(nonatomic, assign) BOOL showConsole;
+
+- (void)showDelayReloadTipWithSeconds:(NSInteger)seconds;
+- (void)removeDelayReloadTip;
+
+/// 获取当前的 hierarchyView
+- (LKHierarchyView *)currentHierarchyView;
+
+#pragma mark - Tutorials
+
+- (v
