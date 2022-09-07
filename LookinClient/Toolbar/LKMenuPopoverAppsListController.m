667a031ea01e9319619463d7e32771f8932ab2b6
shitload <111417421+shitload@users.noreply.github.com>
2022-09-07 19:26:45 +0000

    \ufeff version info: update

--- LookinClient/Toolbar/LKMenuPopoverAppsListController.m
+++ LookinClient/Toolbar/LKMenuPopoverAppsListController.m
@@ -18,4 +18,20 @@
 
 @property(nonatomic, strong) LKLabel *titleLabel;
 @property(nonatomic, strong) LKLabel *subtitleLabel;
-@prop
+@property(nonatomic, strong) LKTextControl *tutorialControl;
+
+@end
+
+@implementation LKMenuPopoverAppsListController {
+    CGFloat _appViewInterSpace;
+    NSEdgeInsets _insets;
+    CGFloat _titleMarginBottom;
+    CGFloat _subtitleMarginBottom;
+}
+
+- (instancetype)initWithApps:(NSArray<LKInspectableApp *> *)apps source:(MenuPopoverAppsListControllerEventSource)source {
+    if (self = [self init]) {
+        _insets = NSEdgeInsetsMake(9, 18, 35, 14);
+        _titleMarginBottom = 3;
+        _subtitleMarginBottom = 5;
+        _appViewInterSpace = 1
