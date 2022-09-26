37aff52d48ec610890036745eac215f9553872c0
shitload <111417421+shitload@users.noreply.github.com>
2022-09-26 12:02:31 +0000

    Updates based on feedback

--- LookinClient/Toolbar/LKWindowToolbarAppButton.m
+++ LookinClient/Toolbar/LKWindowToolbarAppButton.m
@@ -2,4 +2,20 @@
 //  LKWindowToolbarAppView.m
 //  LookinClient
 //
-//  Created by 
+//  Created by 李凯 on 2020/6/14.
+//  Copyright © 2020 hughkli. All rights reserved.
+//
+
+#import "LKWindowToolbarAppButton.h"
+#import "LookinAppInfo.h"
+
+@interface LKWindowToolbarAppButton ()
+
+@property(nonatomic, strong) NSImageView *appImageView;
+@property(nonatomic, strong) LKLabel *appNameLabel;
+@property(nonatomic, strong) NSImageView *sepImageView;
+@property(nonatomic, strong) NSImageView *deviceImageView;
+@property(nonatomic, strong) LKLabel *deviceLabel;
+
+@property(nonatomic, assign) CGFloat appImageWidth;
+@property(nonatomic, copy) NSArray<NSNumber *> *spaces;
