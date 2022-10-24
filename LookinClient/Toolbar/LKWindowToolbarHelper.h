bc5ea470b94167adf69f1d3929981fc633b12745
shitload <111417421+shitload@users.noreply.github.com>
2022-10-24 18:26:25 +0000

    bump version up

--- LookinClient/Toolbar/LKWindowToolbarHelper.h
+++ LookinClient/Toolbar/LKWindowToolbarHelper.h
@@ -2,4 +2,16 @@
 //  LKWindowToolbarHelper.h
 //  Lookin
 //
-//  C
+//  Created by Li Kai on 2019/5/8.
+//  https://lookin.work
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSToolbarItemIdentifier const LKToolBarIdentifier_Dimension;
+extern NSToolbarItemIdentifier const LKToolBarIdentifier_Scale;
+extern NSToolbarItemIdentifier const LKToolBarIdentifier_Rotation;
+extern NSToolbarItemIdentifier const LKToolBarIdentifier_Setting;
+extern NSToolbarItemIdentifier const LKToolBarIdentifier_Reload;
+extern NSToolbarItemIdentifier const LKToolBarIdentifier_App;
+extern NSToolbarItemIdentifier const LKToolBarIden
