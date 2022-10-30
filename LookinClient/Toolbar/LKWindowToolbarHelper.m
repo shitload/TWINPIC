fb32bb63e08400c5fd88f603f18567bef0f1dcda
shitload <111417421+shitload@users.noreply.github.com>
2022-10-30 13:53:47 +0000

    optimize images

--- LookinClient/Toolbar/LKWindowToolbarHelper.m
+++ LookinClient/Toolbar/LKWindowToolbarHelper.m
@@ -12,4 +12,14 @@
 #import "LKAppsManager.h"
 #import "LKNavigationManager.h"
 #import "LookinPreviewView.h"
-#import "LKWi
+#import "LKWindowToolbarScaleView.h"
+#import "LKWindowToolbarAppButton.h"
+
+NSToolbarItemIdentifier const LKToolBarIdentifier_Dimension = @"0";
+NSToolbarItemIdentifier const LKToolBarIdentifier_Scale = @"1";
+NSToolbarItemIdentifier const LKToolBarIdentifier_Setting = @"2";
+NSToolbarItemIdentifier const LKToolBarIdentifier_Reload = @"3";
+NSToolbarItemIdentifier const LKToolBarIdentifier_App = @"5";
+NSToolbarItemIdentifier const LKToolBarIdentifier_AppInReadMode = @"12";
+NSToolbarItemIdentifier const LKToolBarIdentifier_Add = @"13";
+NSToolbarItemIdentif
