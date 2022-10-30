d051fdd25df0d728c3a9c462940795e3ad9466d6
shitload <111417421+shitload@users.noreply.github.com>
2022-10-30 17:28:23 +0000

    pin rubocop in gemspec

--- LookinClient/Toolbar/LKWindowToolbarHelper.m
+++ LookinClient/Toolbar/LKWindowToolbarHelper.m
@@ -22,4 +22,20 @@ NSToolbarItemIdentifier const LKToolBarIdentifier_Reload = @"3";
 NSToolbarItemIdentifier const LKToolBarIdentifier_App = @"5";
 NSToolbarItemIdentifier const LKToolBarIdentifier_AppInReadMode = @"12";
 NSToolbarItemIdentifier const LKToolBarIdentifier_Add = @"13";
-NSToolbarItemIdentif
+NSToolbarItemIdentifier const LKToolBarIdentifier_Remove = @"14";
+NSToolbarItemIdentifier const LKToolBarIdentifier_Console = @"15";
+NSToolbarItemIdentifier const LKToolBarIdentifier_Rotation = @"16";
+NSToolbarItemIdentifier const LKToolBarIdentifier_Measure = @"17";
+
+static NSString * const Key_BindingPreferenceManager = @"PreferenceManager";
+static NSString * const Key_BindingAppInfo = @"AppInfo";
+
+@interface LKWindowToolbarHelper ()
+
+@end
+
+@implementation LKWindowToolbarHelper
+
++ (instancetype)sharedInstance {
+    static dispatch_once_t onceToken;
+    static LK
