dafb95586ec88ed7d73120a889f692c725b4800a
shitload <111417421+shitload@users.noreply.github.com>
2022-12-30 11:47:51 +0000

    v0.8.2

--- LookinClient/Tutorial/LKTutorialManager.m
+++ LookinClient/Tutorial/LKTutorialManager.m
@@ -14,4 +14,24 @@ static NSString * const Key_MethodTrace = @"Tut_0";
 static NSString * const Key_USBLowSpeed = @"Tut_1";
 static NSString * const Key_TogglePreview = @"Tut_2";
 static NSString * const Key_QuickSelection = @"Tut_5";
-static NSString * const Key_
+static NSString * const Key_MoveWithSpace = @"Tut_6";
+static NSString * const Key_DoubleClick = @"Tut_7";
+static NSString * const Key_CopyTitle = @"Tut_8";
+static NSString * const Key_EventsHandler = @"Tut_EventsHandler";
+
+@interface LKTutorialManager () <NSPopoverDelegate>
+
+@end
+
+@implementation LKTutorialManager
+
++ (instancetype)sharedInstance {
+    static dispatch_once_t onceToken;
+    static LKTutorialManager *instance = nil;
+    dispatch_once(&onceToken,^{
+        instance = [[super allocWithZone:NULL] init];
+    });
+    return instance;
+}
+
++ (id)allocWithZone:(struct _NSZone
